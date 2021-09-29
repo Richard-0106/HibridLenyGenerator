d1430caa7c4a58b521804dddc2cf105fc8783df1
Richárd <alex19950106@gmail.com>
2021-09-29 19:34:19 +0200

    html css letrehozasa, json file letrehozasa

---
+++ js.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    const tanulokTomb=[];
+    const allatokTomb=[]
+
+    $.ajax(
+        {
+          url:"adatok.json",
+           success : function(result){
+             result.adatok.forEach((value)=>{allatokTomb.push(value)})    
+            }
+        }
+        );
+        $.ajax(
+            {
+            url:"osztaly.json",
+             success : function(result){
+               result.osztaly.forEach((value)=>{tanulokTomb.push(value)})    
+             }
+        }
+      );
+      function tablazatletrehozas(){
+
+      }
+  });
